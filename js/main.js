7b41c0ef66692883909b05b4f0ead81219853aaa
alicjaada83 <alicja.adamczyk83@gmail.com>
2023-02-20 19:45:14 +0100

    odpowiedz1

---
+++ js/main.js
@@ -0,0 +1,8 @@
+dodaj.onclick = function() {
+    let paragraf = document.getElementById('tekst');
+    paragraf.textContent='jakis tekst co ma sie wyswietlic';
+}
+usuń.onclick = function() {
+    let paragraf = document.getElementById('tekst');
+    paragraf.textContent='';
+}
